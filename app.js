c06ab52b3ae29aef388e7d122a528239bef8d534
Elwoodbond <115675969+Elwoodbond@users.noreply.github.com>
2023-04-26 15:10:55 -0400

    added form to list.ejs

--- app.js
+++ app.js
@@ -1,45 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+
+let items = ["buy food", "cook food", "eat food"];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
 
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-
-switch (currentDay) {
-    case 0:
-        day = "Sunday";
-        break;
-    case 1:
-        day = "Monday";
-        break;
-    case 2:
-        day = "Tuesday";
-        break;
-    case 3:
-        day = "Wednesday";
-        break;
-    case 4:
-        day = "Thursday";
-        break;
-    case 5:
-        day = "Friday";
-        break;
-    case 6:
-        day = "Saturday";
-        break;
-    default:
-        console.log("Error: current day is equal to: " + currentDay);
-    }
+    let today = new Date();
 
 
-    res.render('list', {kindOfDay: day});
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
+
+
+    let day = today.toLocaleDateString('en-US', options);
+
+    res.render('list', {kindOfDay: day, newListItems: items});
+
+
 });
 
-
+app.post('/', function(req, res) {
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect('/');
+});
 
 app.listen(4000, function() {
     console.log('Example app listening on port 3000!');
